e151679c0946d85aaf2260c5bf764244861380fa
albr21 <46101764+albr21@users.noreply.github.com>
2026-06-01 21:15:01 +0200

    feat+fix: add option to skip db download + fix cleanup step when skip db update is false

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -9,6 +9,7 @@ TRIVY_DOCKER_IMAGE_TAG="0.67.2"
 SCAN_SEVERITY="HIGH"
 FAIL_ON_VULNERABILITY="false"
 DOCKER_SOCK_PATH="/var/run/docker.sock"
+SKIP_DB_UPDATE="false"
 
 SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
@@ -28,13 +29,14 @@ while [ "$#" -gt 0 ]; do
     --output-filename) OUTPUT_FILENAME="${2:-$OUTPUT_FILENAME}"; shift 2 ;;
     --trivy-docker-image-name) TRIVY_DOCKER_IMAGE_NAME="${2:-$TRIVY_DOCKER_IMAGE_NAME}"; shift 2 ;;
     --trivy-docker-image-tag) TRIVY_DOCKER_IMAGE_TAG="${2:-$TRIVY_DOCKER_IMAGE_TAG}"; shift 2 ;;
+    --skip-db-update) SKIP_DB_UPDATE="${2:-$SKIP_DB_UPDATE}"; shift 2 ;;
     *) echo "::error::Invalid option $1"; exit 1 ;;
   esac
 done
 
 # === Validate Arguments ===
 if [ -z "$IMAGE_NAME" ] || [ -z "$DOCKER_REGISTRY" ]; then
-  echo "::error::Usage: $0 --docker-registry <docker_registry> --image-name <image_name> [--image-tag <image_tag>] [--scan-severity <scan_severity>] [--fail-on-vulnerability <fail_on_vulnerability>] [--docker-sock <docker_sock_path>] [--output-directory <OUTPUT_DIRECTORY>] [--output-filename <output_filename>]"
+  echo "::error::Usage: $0 --docker-registry <docker_registry> --image-name <image_name> [--image-tag <image_tag>] [--scan-severity <scan_severity>] [--fail-on-vulnerability <fail_on_vulnerability>] [--docker-sock <docker_sock_path>] [--output-directory <OUTPUT_DIRECTORY>] [--output-filename <output_filename>] [--trivy-docker-image-name <trivy_docker_image_name>] [--trivy-docker-image-tag <trivy_docker_image_tag>] [--skip-db-update <skip_db_update>]"
   exit 1
 fi
 
@@ -77,7 +79,8 @@ if [ -n "$SECURITY_LEVELS" ]; then
     image --image-src docker "$IMAGE" \
     --format json \
     --output "/results/$OUTPUT_FILENAME.json" \
-    --severity $SECURITY_LEVELS
+    --severity $SECURITY_LEVELS \
+    $( [ "$SKIP_DB_UPDATE" = "true" ] && echo "--skip-db-update")
   SCAN_EXIT_CODE=$(echo $?)
 else
   echo "::warning::No valid severity levels provided. Skipping scan."
@@ -88,8 +91,10 @@ set -e
 
 # === Cleanup ===
 echo "🧹 Cleaning up..."
-docker image remove "$IMAGE"
-docker image remove "$TRIVY_IMAGE"
+docker rmi -f "$IMAGE" >/dev/null 2>&1 || true
+docker rmi -f "$TRIVY_IMAGE" >/dev/null 2>&1 || true
+docker image prune -f >/dev/null 2>&1 || true
+docker builder prune -f >/dev/null 2>&1 || true
 echo "✅ Cleanup completed!"
 
 # === Exit with error if vulnerabilities are found and fail-on-vulnerability is true ===
